df57dff89341e0f609c3da2852f2dffc0a63dee2
XiNGRZ <hi@xingrz.me>
2021-09-16 14:09:38 +0800

    Add auto-update support

--- awsl.user.js
+++ awsl.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         AWSL
 // @namespace    https://github.com/xingrz
-// @version      0.1.2
+// @version      0.2.0
 // @description  Auto AWSLing
 // @author       XiNGRZ <hi@xingrz.me>
 // @license      WTFPL
 // @match        https://weibo.com/*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/xingrz/awsl/master/awsl.meta.js
 // @downloadURL  https://raw.githubusercontent.com/xingrz/awsl/master/awsl.user.js
 // @homepageURL  https://github.com/xingrz/awsl
 // @supportURL   https://github.com/xingrz/awsl/issues
