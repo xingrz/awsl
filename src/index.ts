2534e880b14a7ee56299ee3f439b9d6b7c8f87f7
XiNGRZ <hi@xingrz.me>
2024-02-15 13:41:05 +0800

    Drop nav items mod
    * Not working as expected

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import './mods/fast_forward';
-import './mods/home_router';
-import './mods/nav_items';
 import './mods/no_retweet_menu';
 import './mods/remove_ads';
 import './mods/user_remark';
--- src/mods/nav_items.ts
+++
@@ -1,9 +0,0 @@
-import { style, attrs, $, observe } from '../utils/dom';
-
-observe(document.body, function adjustNavItems(): void {
-  const navAll = $(document, '.Nav_inner_1QCVO a.ALink_none_1w6rm[href="/"]:not([awsl="yes"])');
-  if (navAll) {
-    style(navAll, { 'display': 'none' });
-    attrs(navAll, { 'awsl': 'yes' });
-  }
-});
