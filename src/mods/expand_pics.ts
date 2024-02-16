98b7a0f7ace2bc49c49e9b2337f09f762abd2fc1
XiNGRZ <hi@xingrz.me>
2024-02-16 16:19:13 +0800

    Expand pictures only on mouse hover to keep smooth scrolling

--- src/mods/expand_pics.ts
+++ src/mods/expand_pics.ts
@@ -1,4 +1,4 @@
-import { $$, append, attrs, create, observe, on } from '../utils/dom';
+import { $, $$, append, attrs, create, observe, on, style } from '../utils/dom';
 import { INode, VueHTMLElement } from '../utils/vue';
 
 interface IPictureBox {
@@ -36,55 +36,67 @@ observe(document.body, function expandPics(): void {
       continue;
     }
 
-    const pics = context.pics.slice(9);
-    if (pics.length == 0) return;
-
-    for (const mask of $$(container, '.picture_mask_20G3v')) {
-      mask.remove();
-    }
-
-    for (const [idx, pic] of pics.entries()) {
-      const inlineBlock = append(container, () => create<HTMLElement>('div', [
-        'woo-box-item-inlineBlock',
-        'picture_item_3zpCn',
-        'picture_cursor_h5pJF',
-      ], {
-        style: {
-          'padding-left': '0.25rem',
-          'padding-top': '0.25rem',
-          'flex-grow': '0',
-        },
-      }));
-
-      const square = append(inlineBlock, () => create('div', [
-        'woo-picture-main',
-        'woo-picture-square',
-        'woo-picture-hover',
-        'picture_pic_eLDxR',
-      ]));
-
-      const img = create('img', ['picture_focusImg_1z5In'], {
-        attrs: { 'src': pic.url },
+    const mask = $(container, '.picture_mask_20G3v');
+    if (mask) {
+      on(mask, 'mouseenter', () => {
+        style(mask, { 'display': 'none' });
+        expand(container, context);
+        window.dispatchEvent(new Event('resize'));  // Force page relayout
       });
 
-      on(img, 'click', (ev) => context.$emit('picture-tap', {
-        index: 9 + idx,
-        pics: context.pics,
-        content: context.$attrs.content,
-      }, ev, img));
-
-      // TODO
-      // if (pic.focus_point) {
-      //   if (pic.focus_point.width > pic.focus_point.height) {
-      //   } else if (pic.focus_point.width < pic.focus_point.height) {
-      //   }
-      // }
-
-      append(square, () => create('div', ['woo-picture-hoverMask']));
-      append(square, () => create('div', ['woo-picture-slot'], {}, [() => img]));
+      on(container, 'mouseleave', () => {
+        style(mask, { 'display': '' });
+        for (const item of $$(container, '.awsl-picbox-item')) {
+          item.remove();
+        }
+        window.dispatchEvent(new Event('resize'));  // Force page relayout
+      });
     }
-
-    // Force page relayout
-    window.dispatchEvent(new Event('resize'));
   }
 });
+
+function expand(container: HTMLElement, context: IPictureBox): void {
+  const pics = context.pics.slice(9);
+
+  for (const [idx, pic] of pics.entries()) {
+    const inlineBlock = append(container, () => create<HTMLElement>('div', [
+      'awsl-picbox-item',
+      'woo-box-item-inlineBlock',
+      'picture_item_3zpCn',
+      'picture_cursor_h5pJF',
+    ], {
+      style: {
+        'padding-left': '0.25rem',
+        'padding-top': '0.25rem',
+        'flex-grow': '0',
+      },
+    }));
+
+    const square = append(inlineBlock, () => create('div', [
+      'woo-picture-main',
+      'woo-picture-square',
+      'woo-picture-hover',
+      'picture_pic_eLDxR',
+    ]));
+
+    const img = create('img', ['picture_focusImg_1z5In'], {
+      attrs: { 'src': pic.url },
+    });
+
+    on(img, 'click', (ev) => context.$emit('picture-tap', {
+      index: 9 + idx,
+      pics: context.pics,
+      content: context.$attrs.content,
+    }, ev, img));
+
+    // TODO
+    // if (pic.focus_point) {
+    //   if (pic.focus_point.width > pic.focus_point.height) {
+    //   } else if (pic.focus_point.width < pic.focus_point.height) {
+    //   }
+    // }
+
+    append(square, () => create('div', ['woo-picture-hoverMask']));
+    append(square, () => create('div', ['woo-picture-slot'], {}, [() => img]));
+  }
+}
