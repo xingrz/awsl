8cc2f30da93e328d51a453abf1fb39eccba29c80
XiNGRZ <hi@xingrz.me>
2024-02-17 13:59:20 +0800

    Show remarks of retweets

--- src/mods/user_remark.ts
+++ src/mods/user_remark.ts
@@ -1,4 +1,4 @@
-import { $, $$, append, attrs, create, html, observe } from '../utils/dom';
+import { $, $$, $H, append, attrs, create, html, observe } from '../utils/dom';
 import { IVNodeContext, VueHTMLElement } from '../utils/vue';
 
 interface IUserInfo {
@@ -12,6 +12,7 @@ interface IUserInfo {
   profile_url: string;
   remark: string;
   screen_name: string;
+  verified: boolean;
 }
 
 interface IStatusContext {
@@ -21,6 +22,16 @@ interface IStatusContext {
   text: string;
   time: string;
   userInfo: IUserInfo;
+}
+
+interface IRetweetContext {
+  data: {
+    id: string;
+    region_name: string;
+    source: string;
+    text: string;
+    user: IUserInfo;
+  };
 }
 
 const SPLITTEER = '<span style="border-right: 1px solid var(--w-off-border); margin: 0 0.5em;"></span>';
@@ -46,6 +57,51 @@ observe(document.body, function userRemark(): void {
     }
     if (context.region_name) {
       info.push(context.region_name);
+    }
+
+    append(container, () => create('div', [], {
+      style: {
+        'color': 'var(--w-sub)',
+        'font-size': '80%',
+        'font-weight': 'normal',
+        'margin-left': '0.5em',
+      },
+      html: info.join(SPLITTEER),
+    }));
+  }
+});
+
+observe(document.body, function userRemark(): void {
+  const reTextNames = $$<VueHTMLElement<IVNodeContext<IRetweetContext>>>(document, '.detail_reText_30vF1 > div > .woo-box-flex:not([awsl-infobox="yes"])');
+  for (const container of reTextNames) {
+    attrs(container, { 'awsl-infobox': 'yes' });
+
+    const data = container.__vue__?.$vnode.context.data;
+    if (!data) continue;
+
+    const ctx = $H<{
+      nick: HTMLElement;
+      verify: HTMLElement;
+    }>(container, {
+      nick: '.detail_nick_u-ffy',
+      verify: '.detail_verify_1GOx9',
+    });
+    if (!ctx) continue;
+
+    html(ctx.nick, data.user.screen_name);
+    if (!data.user.verified) {
+      ctx.verify.remove();
+    }
+
+    const info: string[] = [];
+    if (data.user.remark) {
+      info.push(`备注：${data.user.remark}`);
+    }
+    if (data.user.follow_me) {
+      info.push(data.user.following ? '互相关注' : '关注了我');
+    }
+    if (data.region_name) {
+      info.push(data.region_name);
     }
 
     append(container, () => create('div', [], {
