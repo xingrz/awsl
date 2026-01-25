992ef94ff03b9f321c979975bf4230dc7fc09731
XiNGRZ <hi@xingrz.me>
2026-01-25 11:56:37 +0800

    Drop removal of ADs

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import './mods/expand_pics';
 import './mods/expand_replies';
 import './mods/fast_forward';
 import './mods/logo_click';
-import './mods/remove_ads';
-import './mods/remove_promotion';
 import './mods/settings';
 import './mods/user_remark';
 
--- src/mods/remove_ads.ts
+++
@@ -1,8 +0,0 @@
-import { $$, observe } from '../utils/dom';
-
-observe(document.body, function removeAds(): void {
-  const ads = $$(document, '.TipsAd_wrap_3QB_0');
-  for (const ad of ads) {
-    ad.remove();
-  }
-});
